6119ec6f79030005df0f0f9641fbcf8beced3e11
plamen <plamenvt@gmail.com>
2015-08-15 19:38:32 +0300

    added 2 and 3

---
+++ 15. Development Tools/Homework/2/DevelopmentToolT4/T4Template/Template.cs
@@ -0,0 +1,14 @@
+﻿namespace T4Template
+{
+    using System;
+    using System.Linq;
+
+    public static class Template
+    {
+        public static void Main()
+        {
+            var Tom = new Cat { Name3 = "Tom"};
+            Console.WriteLine(Tom.Name3);
+        }
+    }
+}
---
+++ 15. Development Tools/Homework/2/DevelopmentToolT4/T4Template/cats.cs
@@ -0,0 +1,11 @@
+﻿
+public class Cat
+{
+public string Name1 {get;set;}
+public string Name2 {get;set;}
+public string Name3 {get;set;}
+public string Name4 {get;set;}
+ public void Method()
+    {
+    }
+}
